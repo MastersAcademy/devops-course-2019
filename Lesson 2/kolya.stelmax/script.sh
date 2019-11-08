b44ac4768f17d7f0f50e2eaf5d208e399f2908e1
KolyaStelmax <kolyastelmax763@gmail.com>
2019-11-08 17:07:15 +0200

    Task

--- Lesson 2/kolya.stelmax/script.sh
+++ Lesson 2/kolya.stelmax/script.sh
@@ -13,9 +13,10 @@ fi
 exit 0;
 
 sudo apt install curl gnupg2 ca-certificates lsb-release;
-echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" |sudo tee /$
+echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
 curl -fsSL https://nginx.org/keys/nginx_signing.key |sudo apt-key add -
 apt-key fingerprint ABF5BD827BD9BF62 #key authenticity
+sudo apt update
 sudo apt install -y  nginx=1.14.2*
 
 echo `nginx -v`;
