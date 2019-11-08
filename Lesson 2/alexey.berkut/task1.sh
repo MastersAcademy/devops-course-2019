296490a2a9942994eaf9f8417860d44d707e8004
Alexey <44679993+AlBerkut@users.noreply.github.com>
2019-11-08 18:54:28 +0200

    Update task1.sh

--- Lesson 2/alexey.berkut/task1.sh
+++ Lesson 2/alexey.berkut/task1.sh
@@ -1,25 +1,19 @@
  
 #!/bin/bash
-if [[ "$EUID" -ne 0 ]] # EUID 0 - root for the current process
+if [[ "$EUID" -ne 0 ]] # root check
 then
 	echo "Please run as root";
 	exit 0;
 fi
 
-app=`pacman -Q | grep -c "^nginx\s"`; # for Arch
-# app=`apt list --installed | grep -c nginx`; # for Ubuntu
-		# grep -c return a count of matching lines for input file.
-		# If Nginx is installed, then the number of lines
-		# where it is mentioned is returned. (1 or more)
+apt list --installed | grep -q nginx;
 
-if [[ $app -eq 0 ]]
+if [[ $? != 0 ]]
 then
 	echo "Nginx web server is not install.";
 else
 	echo `nginx -v`;
 	echo -e "\033[31;4mRemove\033[0m Nginx...";
-
-	pacman -R nginx; # for Arch
-	# apt remove nginx*; # for Ubuntu
+	apt remove -y nginx*;
 fi
 exit 0;
