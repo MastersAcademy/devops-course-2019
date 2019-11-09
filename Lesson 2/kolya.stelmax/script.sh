3b1c179142c9ce427984615dde648064404d409a
KolyaStelmax <kolyastelmax763@gmail.com>
2019-11-09 20:56:16 +0200

    Task

--- Lesson 2/kolya.stelmax/script.sh
+++ Lesson 2/kolya.stelmax/script.sh
@@ -10,7 +10,6 @@ echo "nxing is not installed";
 apt remove nginx;
 
 fi
-exit 0;
 
 sudo apt install curl gnupg2 ca-certificates lsb-release;
 echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
