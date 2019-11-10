5573691b74f718cdc543f504f0bdb1f403492e81
Olha Bezpalchuk <olha.bezpalchuk@gmail.com>
2019-11-10 17:46:48 +0000

    minor fix

--- Lesson 2/olha.bezpalchuk/script.sh
+++ Lesson 2/olha.bezpalchuk/script.sh
@@ -31,8 +31,6 @@ echo 'installing nginx 1.14.2...'
 apt install curl gnupg2 ca-certificates lsb-release
 echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
     | sudo tee /etc/apt/sources.list.d/nginx.list
-echo "deb http://nginx.org/packages/mainline/ubuntu `lsb_release -cs` nginx" \
-    | sudo tee /etc/apt/sources.list.d/nginx.list
 curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
 apt-key fingerprint ABF5BD827BD9BF62
 apt-get -y update
