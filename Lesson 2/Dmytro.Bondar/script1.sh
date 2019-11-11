92f676c5d21e8954aa4cde7b5c267e4b2342fe5e
mrwhite92 <tijay92@gmail.com>
2019-11-11 16:55:10 +0200

    correction work_try3

--- Lesson 2/Dmytro.Bondar/script1.sh
+++ Lesson 2/Dmytro.Bondar/script1.sh
@@ -24,7 +24,7 @@ fi
 echo "Installing nginx"
 sudo apt install curl gnupg2 ca-certificates lsb-release --yes
 echo "deb [arch=amd64] http://nginx.org/packages/ubuntu bionic nginx"  | tee /etc/apt/sources.list.d/nginx.list
-curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
+curl -fsSL https://nginx.org/keys/nginx_signing.key | apt-key add -
 apt update
 apt install nginx=1.14.2* --yes
 #Task 2.1
@@ -40,7 +40,7 @@ service nginx restart
 curl 127.0.0.1 |  grep -om 1 "Welcome to nginx"
 #Task 3
 LRED='\033[1;31m'
-echo -n
+echo -n 
 awk '{print "Nginx main process have a PID:"; print $0}' /var/run/nginx.pid #NGINX main process PID
-echo -en "Nginx number of 'worker' processes: $LRED $(ps -aux | grep nginx | grep -c worker)$LRED" #Number s in bold red color
+echo  "Nginx number of 'worker' processes: $LRED $(ps -aux | grep nginx | grep -c worker)$LRED" #Number s in bold red color
 
