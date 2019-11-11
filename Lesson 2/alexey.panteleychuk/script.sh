70b680a9259e577512506ecb89f0a73a13db8800
AlexeyP <alexeyp@mageside.com>
2019-11-11 17:38:58 +0200

    Without 3 task

--- Lesson 2/alexey.panteleychuk/script.sh
+++ Lesson 2/alexey.panteleychuk/script.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
 set -x
+id=$(id -u);
+if [ "$id" -ne "0" ]
+then echo "Run this file by sudo";
+exit 0;
+fi
 
- echo "enter your package name";
-    read name;
+name="nginx";
+dpkg -l $name | grep -w install ;
 
-    dpkg -s $name  1>/dev/null;
-        if [ $? -ne 0 ]; then
-            echo $name "not installed" && sudo apt-get update && sudo apt-get install $name
-          else
-            dpkg -l $name && sudo apt remove -y $name;
-        fi
+if [ "$?" -ne "0" ]
+then echo $name "doesn't install in your system" &&
+apt update && apt install curl gnupg2 ca-certificates lsb-release && apt install -y $name"=1.14.2-1~bionic"
+echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \ | tee /etc/apt/sources.list.d/nginx.list
+curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add - && apt-key fingerprint ABF5BD827BD9BF62 && apt update && apt install -y $name;
 exit 1;
+fi
+cp /etc/nginx/nginx.conf /etc/nginx/sample_nginx.conf
+line=`cat /etc/nginx/nginx.conf | awk '/./{line=$0} END{print NR}'`;
+mv /etc/nginx/nginx.conf default.conf
+sed "$num i\ include /etc/nginx/sites-enabled/\*.conf;" default.conf > nginx.conf
+mkdir /etc/nginx/sites-available/ /etc/nginx/sites-enabled/;
+mv /etc/nginx/conf.d/default.conf /etc/nginx/sites-available/;
+ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/;
+service nginx restart;
+curl -X GET 127.0.0.1 | grep -o "Welcome to nginx!" | head -1;
+
+if [ "$?" -ne "1" ]
+then nv=nginx -v;
+echo $name$nv "Will be remove!!!" && apt remove -y $name;
+exit 2;
+fi
+
+exit 0;
 
