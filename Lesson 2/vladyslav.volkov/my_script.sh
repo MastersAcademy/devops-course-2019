07795add5421617829f062ecd2466d663a095522
Vladyslav Volkov <vva_05@ukr.net>
2019-11-11 18:13:53 +0200

    Lesson 2

--- Lesson 2/vladyslav.volkov/my_script.sh
+++ Lesson 2/vladyslav.volkov/my_script.sh
@@ -16,7 +16,6 @@ apt install curl gnupg2 ca-certificates lsb-release -y
 echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
     | sudo tee /etc/apt/sources.list.d/nginx.list
 curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
-apt-key fingerprint ABF5BD827BD9BF62
 apt update
 apt-get install nginx=1.14.2*
 
