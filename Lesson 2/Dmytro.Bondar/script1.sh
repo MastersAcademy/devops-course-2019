c0c768fd4bb39b62677b3c0c38173b3f2549ed31
mrwhite92 <tijay92@gmail.com>
2019-11-11 16:20:01 +0200

    correction work

--- Lesson 2/Dmytro.Bondar/script1.sh
+++ Lesson 2/Dmytro.Bondar/script1.sh
@@ -23,10 +23,7 @@ fi
 #Task 2
 echo "Installing nginx"
 sudo apt install curl gnupg2 ca-certificates lsb-release --yes
-echo "deb [arch=amd64] http://nginx.org/packages/ubuntu bionic nginx" \
-   		| sudo tee /etc/apt/sources.list.d/nginx.list
-curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
-apt-key fingerprint ABF5BD827BD9BF62
+echo "deb [arch=amd64] http://nginx.org/packages/ubuntu bionic nginx"  | tee /etc/apt/sources.list.d/nginx.list
 apt update
 apt install nginx=1.14.2* --yes
 #Task 2.1
@@ -35,7 +32,7 @@ mkdir sites-available
 mkdir sites-enabled
 sed '/http {/a include /etc/nginx/sites-enabled' /etc/nginx/nginx.conf
 #Task 2.2
-cp /etc/nginx/conf.d/default.conf /etc/nginx/sites-available/
+mv /etc/nginx/conf.d/default.conf /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/
 service nginx restart
 #Task 2.3
@@ -44,5 +41,5 @@ curl 127.0.0.1 |  grep -om 1 "Welcome to nginx"
 LRED='\033[1;31m'
 echo -n
 awk '{print "Nginx main process have a PID:"; print $0}' /var/run/nginx.pid #NGINX main process PID
-echo -en "Nginx number of 'worker' processes: $LRED $(ps -aux | grep nginx | grep -c worker)" #Number s in bold red color
+echo -en "Nginx number of 'worker' processes: $LRED $(ps -aux | grep nginx | grep -c worker)$LRED" #Number s in bold red color
 
