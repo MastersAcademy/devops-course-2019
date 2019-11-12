25a9a1c9030cab380e8df77c4abdcd9a25170a04
AlexeyP <alexeyp@mageside.com>
2019-11-12 12:14:29 +0200

    some code fixed

--- Lesson 2/alexey.panteleychuk/script.sh
+++ Lesson 2/alexey.panteleychuk/script.sh
@@ -2,21 +2,20 @@
 
 set -x
 id=$(id -u);
-if [ "$id" -ne "0" ]
+if [ "$id" -ne 0 ]
 then echo "Run this file by sudo";
 exit 0;
 fi
 
 name="nginx";
-dpkg -l $name | grep -w install ;
+dpkg -l $name | grep -w install 1>/dev/null;
 
-if [ "$?" -ne "0" ]
-then echo $name "doesn't install in your system" &&
-apt update && apt install curl gnupg2 ca-certificates lsb-release && apt install -y $name"=1.14.2-1~bionic"
-echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \ | tee /etc/apt/sources.list.d/nginx.list
-curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add - && apt-key fingerprint ABF5BD827BD9BF62 && apt update && apt install -y $name;
-exit 1;
-fi
+if [ "$?" -ne 0 ]
+then echo $name "doesn't install in your system" && apt update && apt install curl gnupg2 ca-certificates lsb-release
+echo "deb [arch=amd64] http://nginx.org/packages/ubuntu bionic nginx"  | tee /etc/apt/sources.list.d/nginx.list
+curl -fsSL https://nginx.org/keys/nginx_signing.key |  apt-key add -;
+apt update && apt install nginx=1.14.2* --yes;
+
 cp /etc/nginx/nginx.conf /etc/nginx/sample_nginx.conf
 line=`cat /etc/nginx/nginx.conf | awk '/./{line=$0} END{print NR}'`;
 mv /etc/nginx/nginx.conf default.conf
@@ -26,12 +25,14 @@ mv /etc/nginx/conf.d/default.conf /etc/nginx/sites-available/;
 ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/;
 service nginx restart;
 curl -X GET 127.0.0.1 | grep -o "Welcome to nginx!" | head -1;
-
-if [ "$?" -ne "1" ]
-then nv=nginx -v;
-echo $name$nv "Will be remove!!!" && apt remove -y $name;
-exit 2;
-fi
-
 exit 0;
+
+else
+nv=$(nginx -v);
+echo $name$nv "Will be remove!!!" && apt remove -y $name;
+exit 0;
+fi
+exit 0;
+#dpkg -l $name | grep -w install 1>/dev/null;
+
 
