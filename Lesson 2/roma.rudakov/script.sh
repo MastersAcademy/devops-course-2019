acb727a347eee277bbf150a82ac10931c9b08251
Roma Rudakov <rdkv.r.s@gmail.com>
2019-11-20 12:47:46 +0200

    homework1

--- Lesson 2/roma.rudakov/script.sh
+++ Lesson 2/roma.rudakov/script.sh
@@ -37,7 +37,7 @@ fi
 if [ $ngX_numb_of_proc != 0 ]
 then
 	echo "Nginx master process PID: $ngX_master_PID";
-	echo "Nginx worker process count:  $ngX_numb_of_proc ";
+	echo -e "Nginx worker process count: \033[0;31m $ngX_numb_of_proc  ";
 else
 	echo "service nginx stopped";
 fi
