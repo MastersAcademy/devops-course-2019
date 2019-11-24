443758dcb043d677f0aa2585652671223f842dce
icehome.ck <icehome.ck@gmail.com>
2019-11-24 23:41:37 +0200

    4444

--- Lesson 2/kompanioets.oleksandr/ngix_install.sh
+++ Lesson 2/kompanioets.oleksandr/ngix_install.sh
@@ -35,47 +35,38 @@ fi
 	echo "nginx installed"
 
 #  2.1. Добавить папки sites-available и sites-enabled в корень конфигурационной папки nginx. Добавить папку sites-enabled в nginx.conf. 
-#	mkdir /etc/nginx/sites-available
-#	mkdir /etc/nginx/sites-enabled
-#	echo "folders sites-available and sites-enabled is added"
-#	echo "-------------------------------------------------"
-#	echo "add folder sites-enabled in nginx.conf."
-#	echo "include /etc/nginx/sites-enabled/*;" >> /etc/nginx/nginx.conf
-#	sed -i 'include /etc/nginx/sites-enabled/\*.conf' /etc/nginx/nginx.conf
+mkdir /etc/nginx/sites-available
+mkdir /etc/nginx/sites-enabled
+echo "folders sites-available and sites-enabled is added"
+echo "-------------------------------------------------"
+echo "add folder sites-enabled in nginx.conf."
+echo "include /etc/nginx/sites-enabled/*;" >> /etc/nginx/nginx.conf
+sed -i 'include /etc/nginx/sites-enabled/\*.conf' /etc/nginx/nginx.conf
 #  2.2. Перенести файл default.conf в папку sites-available  и сделать симлинк этого файла в папку sites-enabled.
 #	cp /etc/nginx/nginx.conf /etc/nginx/conf.d/default.conf
-#	mv /etc/nginx/conf.d/default.conf /etc/nginx/sites-available/
-#	ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/
-	cd /etc/nginx/;
-	cp nginx.conf conf.d/default.conf
-	mkdir sites-available/ sites-enabled/;
-	sed -i 'include /etc/nginx/sites-enabled/\*.conf' /etc/nginx/nginx.conf
-	# -i for line breaks
-	mv /etc/nginx/conf.d/default.conf /etc/nginx/sites-available
-	#if the file in conf.d you need to use this line of code
-	ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/;
+mv /etc/nginx/conf.d/default.conf /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/
 #    После чего перезапустить nginx.
 
-	service nginx restart
+service nginx restart
 
 # 2.3. Сделать запрос к nginx и получить в результате выполнения скрипта: “Welcome to nginx!”
 
 #	request_to_nginx=`echo wget localhost:80 -O -`
 #	echo "$request_to_nginx"
 
-	curl -X GET 127.0.0.1 | grep -o "Welcome to nginx!" | head -1
+curl -X GET 127.0.0.1 | grep -o "Welcome to nginx!" | head -1
 
 #3. Узнать и вывести PID nginx master process. Сделать это с помощью awk.
 
-	pid_nginx=$(ps -ax | grep nginx | awk '{print $1}')
+pid_nginx=$(ps -ax | grep nginx | awk '{print $1}')
 
 #   Результат форматировать:  “Nginx main process have a PID: {число} “
-	echo "Nginx main process have a PID: $pid_nginx"
+echo "Nginx main process have a PID: $pid_nginx"
 #  3.1. Так же вывести количество запущенных nginx worker process. 
 #    Форматировать так же как в задании 3, но число процессов должны быть красным.
-	RED=$(\033[0;31m) #set red color
-	NC='\033[0m'	#set no color
-	process_count=$(ps -lfC nginx | grep worker | wc -l)
-	echo "Nginx worker process: ${RED}$process_count${NC}"
-	echo "the end script/n______________________________"
-	echo ""
+RED=$(\033[0;31m) #set red color
+NC='\033[0m'	#set no color
+process_count=$(ps -lfC nginx | grep worker | wc -l)
+echo "Nginx worker process: ${RED}$process_count${NC}"
+echo "the end script/n______________________________"
