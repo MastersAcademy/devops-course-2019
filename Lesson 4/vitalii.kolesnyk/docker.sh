255c45117699d41f51364cc73586554ce700386e
adm-crc <57462437+adm-crc@users.noreply.github.com>
2019-11-25 19:32:55 +0200

    Create docker.sh
    У меня вроде как работает но я не уверен((

---
+++ Lesson 4/vitalii.kolesnyk/docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# set -x #отладка
+
+# Задание 1
+
+echo "Дратуті!"
+if dpkg -l | grep ii | grep docker
+then
+        apt purge -y docker*
+fi
+
+apt update
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt-cache policy docker-ce
+apt install docker-ce -y
+useradd -G docker sailor
+su - sailor -c 'docker run -it ubuntu:16.04 bash'
