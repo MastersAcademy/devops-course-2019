93b390b016c30e637978efa054491d0cb682c48a
Oleksandr Bachenko <strechara@gmail.com>
2019-11-26 11:14:51 +0200

    Add key -y, script fixed

--- Lesson 4/oleksandr.bachenko/docker_script.sh
+++ Lesson 4/oleksandr.bachenko/docker_script.sh
@@ -6,7 +6,7 @@ sudo apt-get remove docker docker-engine docker.io containerd runc
 #install new repository
 sudo apt-get update
 
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
