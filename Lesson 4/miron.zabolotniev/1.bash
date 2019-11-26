a89078237093ae0ae123027525c36ab61dcfae8b
MZabolotnev <mirom.zabolotniev@gmail.com>
2019-11-26 09:48:40 +0200

    add hw lesson4

--- Lesson 4/miron.zabolotniev/1.bash
+++ Lesson 4/miron.zabolotniev/1.bash
@@ -1,6 +1,12 @@
-sudo apt-get remove docker docker-engine docker.io containerd runc --yes
-sudo apt-get update
-sudo apt install docker.io --yes
-sudo useradd sailor
-sudo usermod -aG docker sailor
+if [ "$id" -ne 0 ]
+	then echo "Run script by sudo"
+	exit 0;
+fi
+apt-get remove docker docker-engine docker.io containerd runc --yes
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+useradd sailor
+usermod -aG docker sailor
 su sailor -c 'docker run -t -i ubuntu:16.04 /bin/bash '
