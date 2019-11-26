c1ceea84d62f6ffa300ff06aa2cffc1ac28c2126
yaroslav <yaroslavprt@gmail.com>
2019-11-26 12:46:39 +0200

    add -y flag to install scripts;

--- Lesson 4/yaroslav.partyckyi/docker_sailor.sh
+++ Lesson 4/yaroslav.partyckyi/docker_sailor.sh
@@ -10,10 +10,10 @@ echo -e "\n . . . remove/install docker\n"
 
 sudo apt-get update
 sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 echo -e "\n . . . sailor user\n"
 
