9aa5be5bad4b3fe9623cd225e5311797ea5dd456
yaroslav <yaroslavprt@gmail.com>
2019-11-26 22:08:46 +0200

    refactor(lesson 5) better run script;

--- Lesson 5/yaroslav.partyckyi/run.sh
+++ Lesson 5/yaroslav.partyckyi/run.sh
@@ -2,9 +2,21 @@
 
 if [ "$EUID" -ne 0 ]
 then
-  echo ' . . . permission denied, please run as root'
-  exit
+    echo ' . . . permission denied, please run as root'
+    exit
 fi
 
-docker build -t py/nginx . && docker run -p 80:80 py/nginx
- 
+imageName='py/nginx'
+
+docker build -t ${imageName} .
+containerId=$(docker run -p 8181:80 -d $imageName)
+
+echo -e '\n . . . curl to 127.0.0.1:8181\n'
+
+curl 127.0.0.1:8181
+
+echo -e '\n . . . remove container and image\n'
+
+docker container stop ${containerId}
+docker rm ${containerId}
+docker rmi -f ${imageName}
