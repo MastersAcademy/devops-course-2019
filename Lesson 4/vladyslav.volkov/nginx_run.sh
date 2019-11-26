7facfb769b0045c8b4a2fb8fcf1ee42e7d168ded
Vladyslav Volkov <vva_05@ukr.net>
2019-11-26 10:36:14 +0200

    Lesson 4 new fix

--- Lesson 4/vladyslav.volkov/nginx_run.sh
+++ Lesson 4/vladyslav.volkov/nginx_run.sh
@@ -16,7 +16,8 @@ apt-get install curl gnupg2 ca-certificates lsb-release -y
 echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
     | tee /etc/apt/sources.list.d/nginx.list
 curl -fsSL https://nginx.org/keys/nginx_signing.key | apt-key add -
-apt-get install nginx=1.14.2*
+apt-get update
+apt-get install nginx=1.14.2* -y
 
 mkdir /etc/nginx/sites-available /etc/nginx/sites-enabled
 sed -i '15iinclude     /etc/nginx/sites-enabled/\*.conf;' /etc/nginx/nginx.conf
