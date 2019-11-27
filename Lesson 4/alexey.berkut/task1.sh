ff73290e8b2a708448af2f60a2d9bced5bfe7718
AlBerkut <alberkutoff@gmail.com>
2019-11-27 15:47:52 +0200

    Some fix task1

--- Lesson 4/alexey.berkut/task1.sh
+++ Lesson 4/alexey.berkut/task1.sh
@@ -7,16 +7,19 @@ then
 fi
 
 apt remove docker*;
-
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D; # Add offical GPG key
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" # Add Docker repo in APT
-
 apt update;
-apt install -y docker-ce docker-ce-cli containerd.io;
+
+apt install -y apt-transport-https ca-certificates curl software-properties-common;
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -;
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" # Add repo
+apt install -y docker-ce;
 
 adduser sailor --disabled-password --gecos "";
 usermod -aG docker sailor;
-systemctl start docker.service;
+systemctl restart docker.service;
+
+# Test
+docker run hello-world;
 
 su - sailor bash -c "docker run --rm ubuntu:16.04";
 
