ac0fd4ebbbb9d952ebc0ffb57d80ea19c591a0d6
root <root@localhost.localdomain>
2019-12-04 20:03:03 +0200

    commit all edited files

--- Lesson 4/sergey.shevchenko/1ii23.sh
+++ Lesson 4/sergey.shevchenko/1ii23.sh
@@ -12,6 +12,6 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo useradd -G docker -s /bin/bash -d /home/sailor sailor
 cat /etc/passwd |grep sailor
 mkdir /home/sailor
-chown -R sailor /home/sailor
+sudo chown -R sailor /home/sailor
 cp script2.sh /home/sailor/script2.sh
-su - sailor -c "docker run -td --name shevchenko ubuntu:16.04 && docker cp script2.sh shevchenko:/script2.sh && docker exec -it shevchenko ./script2.sh"
+su - sailor -c "docker run -td --name shevchenko ubuntu:18.04 && docker cp script2.sh shevchenko:/script2.sh && docker exec -it shevchenko ./script2.sh"
--- Lesson 4/sergey.shevchenko/script2.sh
+++ Lesson 4/sergey.shevchenko/script2.sh
@@ -4,10 +4,10 @@ apt-get update
 apt install -y curl gnupg2 ca-certificates lsb-release
 echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
     | tee /etc/apt/sources.list.d/nginx.list
-curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
+curl -fsSL https://nginx.org/keys/nginx_signing.key | apt-key add -
 apt-key fingerprint ABF5BD827BD9BF62
 apt update
-apt install -y nginx curl
+apt install -y nginx=1.14.2-1~bionic
 
 cd /etc/nginx &&  mkdir sites-available/ sites-enabled/ 
 
